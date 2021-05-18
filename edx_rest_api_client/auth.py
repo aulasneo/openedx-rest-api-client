4cb744bd02ba9de4f0a615cf94638db5502998d4
Andrés González <andres@aulasneo.com>
2021-05-18 16:38:16 -0300

    Removed deprecated JwtAuth function and dependencies

--- edx_rest_api_client/auth.py
+++ edx_rest_api_client/auth.py
@@ -1,54 +1,7 @@
-import datetime
-
-import jwt
-from edx_django_utils.monitoring import set_custom_attribute
 from requests.auth import AuthBase
 
 
 # pylint: disable=line-too-long
-class JwtAuth(AuthBase):
-    """
-    Attaches JWT Authentication to the given Request object.
-
-    Deprecated:
-        See https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/oauth_dispatch/docs/decisions/0008-use-asymmetric-jwts.rst
-
-    Note: Remove pyjwt dependency when this class is removed.
-
-    """
-
-    def __init__(self, username, full_name, email, signing_key, issuer=None, expires_in=30, tracking_context=None):
-        self.issuer = issuer
-        self.expires_in = expires_in
-        self.username = username
-        self.email = email
-        self.full_name = full_name
-        self.signing_key = signing_key
-        self.tracking_context = tracking_context
-
-    def __call__(self, r):
-        now = datetime.datetime.utcnow()
-        data = {
-            'username': self.username,
-            'full_name': self.full_name,
-            'email': self.email,
-            'iat': now,
-        }
-
-        if self.issuer:
-            data['iss'] = self.issuer
-
-        if self.expires_in:
-            data['exp'] = now + datetime.timedelta(seconds=self.expires_in)
-
-        if self.tracking_context is not None:
-            data['tracking_context'] = self.tracking_context
-
-        set_custom_attribute('deprecated_jwt_signing', 'JwtAuth')
-        r.headers['Authorization'] = 'JWT {jwt}'.format(jwt=jwt.encode(data, self.signing_key))
-        return r
-
-
 class SuppliedJwtAuth(AuthBase):
     """Attaches a supplied JWT to the given Request object."""
 
--- edx_rest_api_client/tests/test_auth.py
+++ edx_rest_api_client/tests/test_auth.py
@@ -1,5 +1,5 @@
 import datetime
-from unittest import mock, TestCase
+from unittest import TestCase
 
 import jwt
 import requests
@@ -8,84 +8,6 @@ import responses
 from edx_rest_api_client import auth
 
 CURRENT_TIME = datetime.datetime(2015, 7, 2, 10, 10, 10)
-
-
-class JwtAuthTests(TestCase):
-
-    def setUp(self):
-        super().setUp()
-
-        self.url = 'http://example.com/'
-        self.username = 'alice'
-        self.full_name = 'αlícє whítє'
-        self.email = 'alice@example.com'
-        self.signing_key = 'edx'
-
-        datetime_patcher = mock.patch.object(
-            auth.datetime, 'datetime',
-            mock.Mock(wraps=datetime.datetime)
-        )
-        mocked_datetime = datetime_patcher.start()
-        mocked_datetime.utcnow.return_value = CURRENT_TIME  # pylint: disable=no-member
-        self.addCleanup(datetime_patcher.stop)
-
-        responses.add(responses.GET, self.url)
-
-    def assert_expected_token_value(self, tracking_context=None, issuer=None, expires_in=None):
-        """ DRY helper. """
-
-        # Mock the HTTP response and issue the request
-        auth_kwargs = {'expires_in': expires_in}
-
-        signing_data = {
-            'username': self.username,
-            'full_name': self.full_name,
-            'email': self.email,
-            'iat': CURRENT_TIME,
-        }
-
-        if issuer:
-            auth_kwargs['issuer'] = issuer
-            signing_data['iss'] = issuer
-
-        if tracking_context:
-            auth_kwargs['tracking_context'] = tracking_context
-            signing_data['tracking_context'] = tracking_context
-
-        if expires_in:
-            signing_data['exp'] = CURRENT_TIME + datetime.timedelta(seconds=expires_in)
-
-        requests.get(
-            self.url,
-            auth=auth.JwtAuth(
-                self.username, self.full_name, self.email, self.signing_key,
-                **auth_kwargs
-            )
-        )
-
-        # Verify the header was set as expected on the request
-        token = jwt.encode(signing_data, self.signing_key)
-        self.assertEqual(responses.calls[0].request.headers['Authorization'], f'JWT {token}')
-
-    @responses.activate
-    def test_headers(self):
-        """ Verify the class adds an Authorization header that includes the correct JWT. """
-        self.assert_expected_token_value()
-
-    @responses.activate
-    def test_tracking_context(self):
-        """ Verify the tracking context is enclosed in the token payload, when specified. """
-        self.assert_expected_token_value(tracking_context={'foo': 'bar'})
-
-    @responses.activate
-    def test_issuer(self):
-        """ Verify that the issuer is enclosed in the token payload, when specified. """
-        self.assert_expected_token_value(issuer='http://example.com/oauth')
-
-    @responses.activate
-    def test_expires_in(self):
-        """ Verify the expiration date is enclosed in the token payload, when specified. """
-        self.assert_expected_token_value(expires_in=60)
 
 
 class BearerAuthTests(TestCase):
@@ -103,7 +25,6 @@ class BearerAuthTests(TestCase):
 
 
 class SuppliedJwtAuthTests(TestCase):
-
     signing_key = 'super-secret'
     url = 'http://example.com/'
 
